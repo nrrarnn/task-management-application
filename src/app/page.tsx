006567a520e6a239254127e469d9b0adcb996e43
Nur Arini <nurarini0302@gmail.com>
2025-07-06 08:55:11 +0700

    feat: implement localstorage persistence for task data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+
+import { useTaskStore } from "@/store/taskStore";
 import TaskBoard from "../components/TaskBoard";
+import { useEffect } from "react";
+import { loadFromStorage, saveToStorage } from "@/utils/localStorage";
 
 export default function Home() {
+  const { tasks, setTasks } = useTaskStore();
+
+  useEffect(() => {
+    const stored = loadFromStorage();
+    if (stored.length > 0) {
+      setTasks(stored);
+    }
+  }, [setTasks]);
+
+  useEffect(() => {
+    saveToStorage(tasks);
+  }, [tasks]);
   return (
     <main className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 p-6 font-poppins">
       <TaskBoard />
--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,4 +1,4 @@
-import { Task } from '../types/task';
+import { Task } from "../types/task";
 
 const STORAGE_KEY = 'task-manager-data';
 
